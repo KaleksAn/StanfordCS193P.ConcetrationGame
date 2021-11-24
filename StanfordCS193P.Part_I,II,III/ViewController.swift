0d8a1608c15d32bd34b661f584e48fb3b80f0420
Alex Kan <alx.kan037@gmail.com>
2021-11-24 11:04:23 +0500

    Initial Commit

---
+++ StanfordCS193P.Part_I,II,III/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StanfordCS193P.Part_I,II,III
+//
+//  Created by Aleksandr Kan on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
